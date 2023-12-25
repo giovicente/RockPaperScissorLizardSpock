864866a36576a65ebe51274cbbfd309e1c9a2b3f
Giovanni <gvn.gentile@gmail.com>
2023-12-25 18:27:33 -0300

    :hammer: refactor: use HashMap instead of switch case and nested if's

--- src/processor/GameRulesImpl.java
+++ src/processor/GameRulesImpl.java
@@ -3,9 +3,7 @@ package processor;
 import core.processor.GameRules;
 import domain.enums.GameSymbol;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 import static processor.Constants.*;
 
@@ -27,41 +25,53 @@ public class GameRulesImpl implements GameRules {
 
     @Override
     public String checksResults(GameSymbol playerMove, GameSymbol cpuMove) {
-
         if (playerMove.equals(cpuMove))
             return TIE_MESSAGE;
 
-        switch (playerMove) {
-            case ROCK -> {
-                if (cpuMove.equals(GameSymbol.SCISSORS) || cpuMove.equals(GameSymbol.LIZARD))
-                    return WINNER_MESSAGE;
-                return LOOSER_MESSAGE;
-            }
-            case PAPER -> {
-                if (cpuMove.equals(GameSymbol.ROCK) || cpuMove.equals(GameSymbol.SPOCK))
-                    return WINNER_MESSAGE;
-                return LOOSER_MESSAGE;
-            }
-            case SCISSORS -> {
-                if (cpuMove.equals(GameSymbol.PAPER) || cpuMove.equals(GameSymbol.LIZARD))
-                    return WINNER_MESSAGE;
-                return LOOSER_MESSAGE;
-            }
-            case LIZARD -> {
-                if (cpuMove.equals(GameSymbol.PAPER) || cpuMove.equals(GameSymbol.SPOCK))
-                    return WINNER_MESSAGE;
-                return LOOSER_MESSAGE;
-            }
-            case SPOCK -> {
-                if (cpuMove.equals(GameSymbol.ROCK) || cpuMove.equals(GameSymbol.SCISSORS))
-                    return WINNER_MESSAGE;
-                return LOOSER_MESSAGE;
-            }
-            default -> {
-                return null;
+        Map<GameSymbol, Map<GameSymbol, String>> winningConditions = winningConditionsMapBuilder();
+        Map<GameSymbol, String> results = winningConditions.get(playerMove);
+
+        for (GameSymbol gameSymbol : results.keySet()) {
+            if (gameSymbol.equals(cpuMove)) {
+                return results.get(gameSymbol);
             }
         }
+
+        return LOOSER_MESSAGE;
     }
+
+    private static Map<GameSymbol, Map<GameSymbol, String>> winningConditionsMapBuilder() {
+        Map<GameSymbol, Map<GameSymbol, String>> winningConditions = new HashMap<>();
+
+        Map<GameSymbol, String> rockWins = new HashMap<>();
+        rockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+        rockWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> paperWins = new HashMap<>();
+        paperWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        paperWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> scissorsWins = new HashMap<>();
+        scissorsWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        scissorsWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> lizardWins = new HashMap<>();
+        lizardWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        lizardWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> spockWins = new HashMap<>();
+        spockWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        spockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+
+        winningConditions.put(GameSymbol.ROCK, rockWins);
+        winningConditions.put(GameSymbol.PAPER, paperWins);
+        winningConditions.put(GameSymbol.SCISSORS, scissorsWins);
+        winningConditions.put(GameSymbol.LIZARD, lizardWins);
+        winningConditions.put(GameSymbol.SPOCK, spockWins);
+
+        return winningConditions;
+    }
+
 
     @Override
     public int checksSymbol(Scanner gameScanner) {
