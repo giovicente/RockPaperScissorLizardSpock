4b46f4d59c6ea94c2f94ea87db8cac83413c514f
Giovanni <gvn.gentile@gmail.com>
2023-12-28 00:52:35 -0300

    :hammer: refactor: isolating winning conditions creation

---
+++ src/domain/WinningConditions.java
@@ -0,0 +1,45 @@
+package domain;
+
+import domain.enums.GameSymbol;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static processor.Constants.WINNER_MESSAGE;
+
+public class WinningConditions {
+
+    private static Map<GameSymbol, Map<GameSymbol, String>> conditions;
+
+    public final static Map<GameSymbol, Map<GameSymbol, String>> getInstance() {
+        conditions = new HashMap<>();
+
+        Map<GameSymbol, String> rockWins = new HashMap<>();
+        rockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+        rockWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> paperWins = new HashMap<>();
+        paperWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        paperWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> scissorsWins = new HashMap<>();
+        scissorsWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        scissorsWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> lizardWins = new HashMap<>();
+        lizardWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        lizardWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> spockWins = new HashMap<>();
+        spockWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        spockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+
+        conditions.put(GameSymbol.ROCK, rockWins);
+        conditions.put(GameSymbol.PAPER, paperWins);
+        conditions.put(GameSymbol.SCISSORS, scissorsWins);
+        conditions.put(GameSymbol.LIZARD, lizardWins);
+        conditions.put(GameSymbol.SPOCK, spockWins);
+
+        return conditions;
+    }
+}
--- src/processor/GameRulesImpl.java
+++ src/processor/GameRulesImpl.java
@@ -1,6 +1,7 @@
 package processor;
 
 import core.processor.GameRules;
+import domain.WinningConditions;
 import domain.enums.GameSymbol;
 
 import java.util.*;
@@ -27,7 +28,7 @@ public class GameRulesImpl implements GameRules {
     public String checksResults(GameSymbol playerMove, GameSymbol cpuMove) {
         if (playerMove.equals(cpuMove)) { return TIE_MESSAGE; }
 
-        Map<GameSymbol, Map<GameSymbol, String>> winningConditions = winningConditionsMapBuilder();
+        Map<GameSymbol, Map<GameSymbol, String>> winningConditions = WinningConditions.getInstance();
         Map<GameSymbol, String> results = winningConditions.get(playerMove);
 
         for (GameSymbol gameSymbol : results.keySet()) {
@@ -71,35 +72,4 @@ public class GameRulesImpl implements GameRules {
         return (playAgain != 'Y' && playAgain != 'N');
     }
 
-    private static Map<GameSymbol, Map<GameSymbol, String>> winningConditionsMapBuilder() {
-        Map<GameSymbol, Map<GameSymbol, String>> winningConditions = new HashMap<>();
-
-        Map<GameSymbol, String> rockWins = new HashMap<>();
-        rockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
-        rockWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
-
-        Map<GameSymbol, String> paperWins = new HashMap<>();
-        paperWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
-        paperWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
-
-        Map<GameSymbol, String> scissorsWins = new HashMap<>();
-        scissorsWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
-        scissorsWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
-
-        Map<GameSymbol, String> lizardWins = new HashMap<>();
-        lizardWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
-        lizardWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
-
-        Map<GameSymbol, String> spockWins = new HashMap<>();
-        spockWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
-        spockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
-
-        winningConditions.put(GameSymbol.ROCK, rockWins);
-        winningConditions.put(GameSymbol.PAPER, paperWins);
-        winningConditions.put(GameSymbol.SCISSORS, scissorsWins);
-        winningConditions.put(GameSymbol.LIZARD, lizardWins);
-        winningConditions.put(GameSymbol.SPOCK, spockWins);
-
-        return winningConditions;
-    }
 }
