475bc9823a8cc28a4d22ca58b65e98239a73d7f9
Giovanni <gvn.gentile@gmail.com>
2023-12-30 21:04:10 -0300

    :white_check_mark: tests: add winning condition getInstance() test

--- src/test/processor/GameRulesImplTest.java
+++ src/test/processor/GameRulesImplTest.java
@@ -1,6 +1,7 @@
 package test.processor;
 
 import core.processor.GameRules;
+import domain.WinningConditions;
 import domain.enums.GameSymbol;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import processor.GameRulesImpl;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -393,5 +396,68 @@ class GameRulesImplTest {
         char continuityAnswer = invalidContinuityChars.charAt(randomIndex.nextInt(invalidContinuityChars.length()));
 
         Assertions.assertTrue(GameRulesImpl.isInvalidContinuityAnswer(continuityAnswer));
+    }
+
+    @Test
+    void createValidWinningConditionsMapTest() {
+        Map<GameSymbol, Map<GameSymbol, String>> expectedWinningConditions = new HashMap<>();
+
+        Map<GameSymbol, String> rockWins = new HashMap<>();
+        rockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+        rockWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> paperWins = new HashMap<>();
+        paperWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        paperWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> scissorsWins = new HashMap<>();
+        scissorsWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        scissorsWins.put(GameSymbol.LIZARD, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> lizardWins = new HashMap<>();
+        lizardWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+        lizardWins.put(GameSymbol.SPOCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> spockWins = new HashMap<>();
+        spockWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+        spockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+
+        expectedWinningConditions.put(GameSymbol.ROCK, rockWins);
+        expectedWinningConditions.put(GameSymbol.PAPER, paperWins);
+        expectedWinningConditions.put(GameSymbol.SCISSORS, scissorsWins);
+        expectedWinningConditions.put(GameSymbol.LIZARD, lizardWins);
+        expectedWinningConditions.put(GameSymbol.SPOCK, spockWins);
+
+        Map<GameSymbol, Map<GameSymbol, String>> actualWinningConditions = WinningConditions.getInstance();
+        Assertions.assertEquals(expectedWinningConditions, actualWinningConditions);
+    }
+
+    @Test
+    void createInvalidWinningConditionsMapTest() {
+        Map<GameSymbol, Map<GameSymbol, String>> unexpectedWinningConditions = new HashMap<>();
+
+        Map<GameSymbol, String> rockWins = new HashMap<>();
+        rockWins.put(GameSymbol.SCISSORS, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> paperWins = new HashMap<>();
+        paperWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> scissorsWins = new HashMap<>();
+        scissorsWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> lizardWins = new HashMap<>();
+        lizardWins.put(GameSymbol.PAPER, WINNER_MESSAGE);
+
+        Map<GameSymbol, String> spockWins = new HashMap<>();
+        spockWins.put(GameSymbol.ROCK, WINNER_MESSAGE);
+
+        unexpectedWinningConditions.put(GameSymbol.ROCK, rockWins);
+        unexpectedWinningConditions.put(GameSymbol.PAPER, paperWins);
+        unexpectedWinningConditions.put(GameSymbol.SCISSORS, scissorsWins);
+        unexpectedWinningConditions.put(GameSymbol.LIZARD, lizardWins);
+        unexpectedWinningConditions.put(GameSymbol.SPOCK, spockWins);
+
+        Map<GameSymbol, Map<GameSymbol, String>> actualWinningConditions = WinningConditions.getInstance();
+        Assertions.assertNotEquals(unexpectedWinningConditions, actualWinningConditions);
     }
 }
